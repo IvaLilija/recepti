776edd6597bdd52ddb15c66e4f153d07a461c318
Iva Arh <IvaLilija>
2024-12-19 18:06:34 +0100

    test

---
+++ city_insert.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once "db.php";
+
+
+$title = $_POST['title'];
+$post_number = $_POST['post_number'];
+
+$sql =  "INSERT INTO cities(title, post_number) VALUES (?, ?)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$title, $post_number]);
+
+//preusmeritev
+header("Location: cities.php");
+?>
